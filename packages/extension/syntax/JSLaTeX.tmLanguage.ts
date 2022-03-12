10dd601caa2fbb0f486b52418761a94fd22a5dbd
Leon Si <contact@leonzalion.com>
2022-03-12 16:32:33 -0500

    fix: extension

--- packages/extension/syntax/JSLaTeX.tmLanguage.ts
+++ packages/extension/syntax/JSLaTeX.tmLanguage.ts
@@ -1,89 +1,47 @@
 export default function getConfigString() {
 	// https://stackoverflow.com/a/34755045
-	function r(regexString: string) {
-		return regexString
-			.split('\n')
-			.map((line) => line.trim())
-			.filter((line) => line !== '') // don't include empty lines
-			.join('');
-	}
+	// function r(regexString: string) {
+	// 	return regexString
+	// 		.split('\n')
+	// 		.map((line) => line.trim())
+	// 		.filter((line) => line !== '') // don't include empty lines
+	// 		.join('');
+	// }
 
 	const ejsBeginTag = '<%[%_=-]?';
 	const ejsEndTag = '[_-]?%>';
 
 	const config = {
 		name: 'JSLaTeX File',
-		scopeName: 'source.jslatex',
+		scopeName: 'text.tex.latex.jslatex',
 		fileTypes: ['jtex'],
 		patterns: [
-			{
-				include: '#ejs',
-			},
-			{
-				include: 'text.tex.latex',
-			},
+			{ include: '#tag-block-comment' },
+			{ include: '#tag-ejs' },
+			{ include: 'text.tex.latex' },
 		],
 		repository: {
-			ejs: [
-				{ include: '#tag-block-comment' },
-				{ include: '#tag-ejs-single-line' },
-				{ include: '#tag-ejs-multi-line' },
-			],
 			// Comments that use the EJS <%# tag
 			'tag-block-comment': {
 				name: 'comment.block.ejs',
+				contentName: 'comment.block.js',
 				begin: '<%#',
 				beginCaptures: {
-					'0': {
-						name: 'punctuation.definition.comment.ejs',
+					'1': {
+						name: 'punctuation.definition.comment.js',
 					},
 				},
 				end: '%>',
 				endCaptures: {
 					'0': {
-						name: 'punctuation.definition.comment.ejs',
+						name: 'punctuation.definition.comment.js',
 					},
 				},
 			},
-			'tag-ejs-single-line': {
-				name: 'meta.tag.metadata.script.ejs',
-				begin: r(String.raw`
-					(${ejsBeginTag})
-					${
-						/* All characters that aren't followed by an ending EJS tag (matches up to an EJS end tag) */ ''
-					}
-					(
-						(?:
-							(?!${ejsEndTag}).
-						)+
-					)
-					(?=${ejsEndTag})
-				`),
-				beginCaptures: {
-					'0': {
-						contentName: 'source.js',
-						name: 'meta.embedded.*',
-					},
-					'1': {
-						// PHP has nice syntax highlighting for embedding (i.e. the <?php ?> tags)
-						name: 'punctuation.section.embedded.begin.php',
-					},
-					'2': {
-						name: 'meta.embedded.ejs',
-						contentName: 'source.js',
-						patterns: [{ include: 'source.js' }],
-					},
-				},
-				end: ejsEndTag,
-				endCaptures: {
-					'0': {
-						name: 'punctuation.section.embedded.end.php',
-					},
-				},
-			},
-			'tag-ejs-multi-line': {
-				contentName: 'source.js',
+			'tag-ejs': {
 				name: 'meta.block.ejs',
+				// The content between the EJS tags is matched as JavaScript
+				contentName: 'source.js',
 				begin: ejsBeginTag,
 				beginCaptures: {
 					'0': {
@@ -92,10 +50,11 @@ export default function getConfigString() {
 				},
 				end: ejsEndTag,
 				endCaptures: {
-					'1': {
-						name: 'punctuation.section.embedded.begin.php',
+					'0': {
+						name: 'punctuation.section.embedded.end.php',
 					},
 				},
+				// Matched against the part between the begin and end matches
 				patterns: [{ include: 'source.js' }],
 			},
 		},
