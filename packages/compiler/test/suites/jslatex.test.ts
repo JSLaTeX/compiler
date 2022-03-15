0a08d9963f7f0498cb4b19178de15c1c7e2f4200
Leon Si <contact@leonzalion.com>
2022-03-15 13:39:01 -0400

    test: snapshots

--- packages/compiler/test/suites/jslatex.test.ts
+++ packages/compiler/test/suites/jslatex.test.ts
@@ -1,6 +1,11 @@
-import { expect, test } from 'vitest';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+import { expect, test, describe } from 'vitest';
 import { outdent } from 'outdent';
+import { join } from 'desm';
 import { compileJsLatex } from '~/index.js';
+
+const fixturesDir = join(import.meta.url, '../fixtures');
 
 test('compiles JSLaTeX', async () => {
 	const jsLatex = outdent.string(String.raw`
@@ -32,4 +37,14 @@ test('compiles JSLaTeX with ramda', async () => {
 			55
 		`)
 	);
+});
+
+describe('compiles fixtures', () => {
+	test('chem.tex', async () => {
+		const chemTex = await fs.promises.readFile(
+			path.join(fixturesDir, 'chem.tex'),
+			'utf8'
+		);
+		expect(await compileJsLatex({ latex: chemTex })).toMatchSnapshot();
+	});
 });
