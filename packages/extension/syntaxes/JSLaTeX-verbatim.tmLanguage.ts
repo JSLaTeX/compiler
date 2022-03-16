1aaff21f7689057ff9088390d5c6d0bcfbc80a95
Leon Si <contact@leonzalion.com>
2022-03-16 16:01:12 -0400

    chore: update vscode-ejs

--- packages/extension/syntaxes/JSLaTeX-verbatim.tmLanguage.ts
+++ packages/extension/syntaxes/JSLaTeX-verbatim.tmLanguage.ts
@@ -2,16 +2,17 @@ import * as vscodeEjs from 'vscode-ejs';
 import { tagDelimiters } from './utils/tag.js';
 
 export default function getConfigString() {
+	const { repository, patterns } = vscodeEjs.getGrammar(tagDelimiters, {
+		languageName: 'ets',
+		sourceLanguageName: 'ts',
+	});
+
 	const config = {
 		name: 'JSLaTeX Verbatim Block',
 		scopeName: 'markup.raw.verbatim.latex.jslatex',
 		injectionSelector: 'L:markup.raw.verbatim.latex',
-		patterns: [
-			{ include: '#tag-block-comment' },
-			{ include: '#single-line-tag-ejs' },
-			{ include: '#tag-ejs' },
-		],
-		repository: vscodeEjs.getRepository(tagDelimiters),
+		patterns,
+		repository,
 	};
 
 	return JSON.stringify(config);
--- packages/extension/syntaxes/JSLaTeX.tmLanguage.ts
+++ packages/extension/syntaxes/JSLaTeX.tmLanguage.ts
@@ -2,17 +2,17 @@ import * as vscodeEjs from 'vscode-ejs';
 import { tagDelimiters } from './utils/tag.js';
 
 export default function getConfigString() {
+	const { repository, patterns } = vscodeEjs.getGrammar(tagDelimiters, {
+		languageName: 'ets',
+		sourceLanguageName: 'ts',
+	});
+
 	const config = {
 		name: 'JSLaTeX File',
 		scopeName: 'text.tex.latex.jslatex',
 		injectionSelector: 'L:text.tex.latex - markup.raw.verbatim.latex',
-		patterns: [
-			{ include: '#tag-block-comment' },
-			{ include: '#single-line-tag-ejs' },
-			{ include: '#tag-ejs' },
-			{ include: 'text.tex.latex' },
-		],
-		repository: vscodeEjs.getRepository(tagDelimiters),
+		patterns: [...patterns, { include: 'text.tex.latex' }],
+		repository,
 	};
 
 	return JSON.stringify(config);
