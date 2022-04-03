dc34f45ea0d57b9197a73f8631d14abfe433765f
Leon Si <contact@leonzalion.com>
2022-04-03 06:28:30 -0400

    chore: prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
-const path = require("path");
-const { defineConfig } = require("eslint-define-config");
+const path = require('path');
+const { defineConfig } = require('eslint-define-config');
 
 module.exports = defineConfig({
-	extends: [require.resolve("@leonzalion/configs/eslint.cjs")],
+	extends: [require.resolve('@leonzalion/configs/eslint.cjs')],
 	parserOptions: {
-		project: path.resolve(__dirname, "./tsconfig.eslint.json"),
+		project: path.resolve(__dirname, './tsconfig.eslint.json'),
 	},
 });
