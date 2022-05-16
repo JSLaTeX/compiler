a8aca8f72332608d442611dba996db529a560558
Leon Si <contact@leonzalion.com>
2022-05-16 15:36:19 -0400

    chore: migrate to lionconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,3 @@
-const path = require('path');
-const { defineConfig } = require('eslint-define-config');
+const createESLintConfig = require('lionconfig/eslint')
 
-module.exports = defineConfig({
-	extends: require.resolve('@leonzalion/configs/eslint.cjs'),
-	parserOptions: {
-		project: path.resolve(__dirname, './tsconfig.eslint.json'),
-	},
-});
+module.exports = createESLintConfig(__dirname);
